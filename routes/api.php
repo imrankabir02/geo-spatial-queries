9d6fde9f86d7cb1ab78375a06858d1ab451d724b
imrankabir02 <imrankabir325@gmail.com>
2025-02-27 18:19:12 +0600

    api installed

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/locations', 'LocationController@store');
+Route::get('/locations/nearby', 'LocationController@nearby');
+
+
